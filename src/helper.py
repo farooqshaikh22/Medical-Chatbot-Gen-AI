7938341b9c9719b06bd7ab86bf1c3cace2e71a1d
Faruq <farooqshaikh22@ymail.com>
2024-02-16 23:09:15 +0530

    helper.py updated

---
+++ src/helper.py
@@ -0,0 +1,32 @@
+from langchain.document_loaders import PyPDFLoader,DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+
+
+#extract data from pdf
+def load_pdf(data_path):
+    loader = DirectoryLoader(
+        path=data_path,
+        glob='*.pdf',
+        loader_cls=PyPDFLoader
+        )
+    documents = loader.load()
+    return documents
+
+
+#creating text chunks
+def text_split(documents):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=20)
+
+    text_chunks = text_splitter.split_documents(documents)
+    
+    return text_chunks
+
+#download embedding model
+def download_huggingface_embeddings():
+    
+    embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2')
+    
+    return embeddings
