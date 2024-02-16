ae16f960f3905d5499a10f9da53f2d6c186980bc
Faruq <farooqshaikh22@ymail.com>
2024-02-16 21:27:04 +0530

    local package created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+      name='Medical Chatbot',
+      version= '0.0.0',
+      author= 'Faruq Shaikh',
+      author_email='farooqshaikh22@ymail.com',
+      packages=find_packages(),
+      install_requires = []
+      )
